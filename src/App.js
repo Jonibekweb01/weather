a68f3de3eef07585b39ce80630c6ccf5a930cdb3
Jonibekweb01 <jonibekqodirovv@gmail.com>
2024-07-05 19:12:12 +0500

    kf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import "./app.css"
+
 function App() {
   return (
     <div className='hero'>
